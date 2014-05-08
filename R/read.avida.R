9483fde68c527b88cab3451415f72abdcdf9a36e
Brian Connelly <bdc@bconnelly.net>
2014-05-07 21:37:51 -0700

    Added NEWS, extract.colnames to read.avida

--- R/read.avida.R
+++ R/read.avida.R
@@ -2,16 +2,23 @@ library(stringr)
 
 #' Read a file in Avida's output format
 #'
-#' \code{read.avida} reads the given file in Avida's output format and returns a data table.
+#' \code{read.avida} reads the given file in Avida's output format and returns a
+#' data table. Column names are assigned from comments within the data file
+#' unless the \code{extract.colnames} parameter is set to \code{FALSE}.
+#' Characters in column names that aren't letters, numbers, underscore, or
+#' period are replaced by underscores.
 #'
 #' @export
 #' @seealso \code{\link{read.table}}
 #' @param file the name of the file which the data are to be read from
+#' @param extract.colnames Boolean indicating whether or not column names should
+#' be extracted from the comments in the data file (\code{TRUE}) or not 
+#' (\code{FALSE}) (default: \code{TRUE}).
 #' @return A data frame (\code{\link{data.frame}}) containing a representation of the data in the file
 #' @examples
 #' data <- read.avida('resource.dat')
 #' 
-read.avida <- function(file)
+read.avida <- function(file, extract.colnames=TRUE)
 {
 	raw <- readLines(file)
 	comments <- grep('^\\s*#', raw, value=TRUE)
@@ -24,7 +31,11 @@ read.avida <- function(file)
     col_names <- str_replace_all(col_names, '[^a-zA-Z0-9_.\\s]', '_')
     
 	data <- read.table(file)
-	names(data) <- col_names
+	
+    if(extract.colnames)
+    {
+        names(data) <- col_names
+    }
 	
 	return(data)
 }
